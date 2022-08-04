dfffe1d2ecb82e34e3fa323ec2d7e39bcb0a808b
NoonLnwza <pubet50@gmail.com>
2022-08-04 14:12:06 +0700

    so hard

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  const Imgprofile = document.getElementById("img-profile");
+  const Gendericon = document.getElementById("span-gender-icon");
+  const Name = document.getElementById("p-name");
+  if (resp.data.results[0].gender == "male") {
+    Gendericon.innerText = "👨";
+  } else {
+    Gendericon.innerText = "👩";
+  }
+  Name.innerText = resp.data.results[0].name;
+  alert(resp.results[0]);
+  Imgprofile.src = resp.data.results[0].picture.large;
 }
+
+callApi();
+
+const btnRandom = document.getElementById("btn-random");
+btnRandom.onclick = async = () => {
+  callApi();
+};
